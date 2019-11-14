15894cdf2ed5a442bfffc903735c0fd48959e0d8
Ivan <belzee92@gmail.com>
2019-11-14 14:56:36 -0500

    optimize for production

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -20,6 +20,14 @@ module.exports = {
       errorDetails: true
     }
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
   plugins: [
     new Dotenv({
       path: ".env"
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,29 @@
 const Dotenv = require("dotenv-webpack");
 const WebpackBar = require("webpackbar");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const PurifyCSSPlugin = require("purifycss-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const glob = require("glob");
+const path = require("path");
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
   optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true
+        }
+      })
+    ],
     splitChunks: {
       cacheGroups: {
         commons: {
@@ -15,10 +34,24 @@ module.exports = {
       }
     }
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  },
   plugins: [
     new Dotenv({
       path: ".env.production"
     }),
-    new WebpackBar({ name: "Buiding Production" })
+    new WebpackBar({ name: "Buiding Production" }),
+    new MiniCssExtractPlugin({ filename: "styles/[name].css" }),
+    new PurifyCSSPlugin({
+      paths: glob.sync(`${path.join(__dirname, "src")}/**/*.js`, {
+        nodir: true
+      })
+    })
   ]
 };
